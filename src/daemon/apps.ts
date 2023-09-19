899902fff03534363e4c70761edd929648f1a5b9
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2023-09-19 10:11:48 +0200

    Wip

--- src/daemon/apps.ts
+++ src/daemon/apps.ts
@@ -1,6 +1,6 @@
-import { getAppDirectory } from './appDirectory'
-import { getSettings, getInstalledApps, saveInstalledApps } from './config'
-import { Variables } from '../types/Variables'
+import {getAppDirectory} from './appDirectory'
+import {getSettings, getInstalledApps, saveInstalledApps} from './config'
+import {Variables} from '../types/Variables'
 
 export const listApps = async (): Promise<Record<string, boolean>> => {
   const settings = getSettings()
@@ -8,13 +8,13 @@ export const listApps = async (): Promise<Record<string, boolean>> => {
   const installedApps = getInstalledApps()
   const appNames = Object.keys(await getAppDirectory(settings.appDirectory))
   return appNames.reduce(
-    (acc, cur) => ({ ...acc, [cur]: !!installedApps[cur] }),
-    {},
+      (acc, cur) => ({...acc, [cur]: !!installedApps[cur]}),
+      {},
   )
 }
 
 export const installApp = async (name: string, variables: Variables) =>
-  saveInstalledApps({ ...getInstalledApps(), [name]: variables || {} })
+    saveInstalledApps({...getInstalledApps(), [name]: variables || {}})
 
 export const removeApp = async (name: string) => {
   const temp = getInstalledApps()
@@ -23,10 +23,13 @@ export const removeApp = async (name: string) => {
 }
 
 export const getAppMetadata = async (name: string) => {
-  const app = { ...(await getAppDirectory(getSettings().appDirectory))[name] }
+  const app = {
+    ...(await getAppDirectory(getSettings().appDirectory))[name],
+    installed: !!getInstalledApps()[name]
+  }
   delete app.logoImage
   return app
 }
 
 export const getAppLogo = async (name: string) =>
-  (await getAppDirectory(getSettings().appDirectory))[name].logoImage || ''
+    (await getAppDirectory(getSettings().appDirectory))[name].logoImage || ''
