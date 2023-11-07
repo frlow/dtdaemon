4e252c73c4608f01947d3a730ba16540f37adeb7
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2023-11-07 21:50:10 +0100

    Wip

--- src/api.ts
+++ src/api.ts
@@ -1,55 +1,61 @@
-import { Settings } from './types/Settings'
-import { AppConfig } from './types/AppDirectory'
-import { Log } from './types/Log'
+import {Settings} from './types/Settings'
+import {AppConfig} from './types/AppDirectory'
+import {Log} from './types/Log'
 
 export type Client = ReturnType<typeof createClient>
+
+const readLog = async (url: string, log: Log) => {
+  const response = await fetch(url)
+  const reader = response.body?.getReader()
+  if (!reader) return undefined
+      ;
+  (async () => {
+    while (true) {
+      const {value, done} = await reader.read()
+      if (done) break
+      log(Buffer.from(value).toString('utf8'))
+    }
+  })()
+  return () => reader.cancel()
+}
+
 export const createClient = (daemonHostUrl: string) => ({
   status: async () =>
-    (await fetch(daemonHostUrl).catch(() => undefined))?.status as number,
+      (await fetch(daemonHostUrl).catch(() => undefined))?.status as number,
   pull: async () => {
-    await fetch(`${daemonHostUrl}/pull`, { method: 'POST' })
+    await fetch(`${daemonHostUrl}/pull`, {method: 'POST'})
   },
 
-  appLog: async (name: string, log: Log) => {
-    const response = await fetch(`${daemonHostUrl}/apps/${name}/log`)
-    const reader = response.body?.getReader()
-    if (!reader) return undefined
-    ;(async () => {
-      while (true) {
-        const { value, done } = await reader.read()
-        if (done) break
-        log(Buffer.from(value).toString('utf8'))
-      }
-    })()
-    return () => reader.cancel()
-  },
+  appLog: (name: string, log: Log) => readLog(`${daemonHostUrl}/apps/${name}/log`, log),
+
+  log: (log: Log) => readLog(`${daemonHostUrl}/log`, log),
 
   update: async () => {
-    await fetch(`${daemonHostUrl}/update`, { method: 'POST' })
+    await fetch(`${daemonHostUrl}/update`, {method: 'POST'})
   },
 
   removeApp: async (name: string) =>
-    await fetch(`${daemonHostUrl}/apps/${name}`, {
-      method: 'DELETE',
-    }),
+      await fetch(`${daemonHostUrl}/apps/${name}`, {
+        method: 'DELETE',
+      }),
 
   installApp: async (name: string, variables: Record<string, string>) =>
-    await fetch(`${daemonHostUrl}/apps/${name}`, {
-      method: 'POST',
-      body: JSON.stringify(variables),
-    }),
+      await fetch(`${daemonHostUrl}/apps/${name}`, {
+        method: 'POST',
+        body: JSON.stringify(variables),
+      }),
 
   getAppLogo: async (name: string) =>
-    await fetch(`${daemonHostUrl}/apps/${name}/logo`).then((r) => r.text()),
+      await fetch(`${daemonHostUrl}/apps/${name}/logo`).then((r) => r.text()),
 
-  listApps: async (): Promise<{installed: boolean, service: boolean, name: string}[]> =>
-    await fetch(`${daemonHostUrl}/apps`).then((r) => r.json()),
+  listApps: async (): Promise<{ installed: boolean, service: boolean, name: string }[]> =>
+      await fetch(`${daemonHostUrl}/apps`).then((r) => r.json()),
 
   getAppMetadata: async (name: string): Promise<AppConfig> =>
-    await fetch(`${daemonHostUrl}/apps/${name}`).then((r) => r.json()),
+      await fetch(`${daemonHostUrl}/apps/${name}`).then((r) => r.json()),
 
   getSettings: async (): Promise<Settings> =>
-    await fetch(`${daemonHostUrl}/settings`).then((r) => r.json()),
+      await fetch(`${daemonHostUrl}/settings`).then((r) => r.json()),
 
   saveSettings: async (settings: Settings) => {
     await fetch(`${daemonHostUrl}/settings`, {
